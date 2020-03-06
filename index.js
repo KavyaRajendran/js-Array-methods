6ed6ac0512ee87a6dee02a4e8a25909a59eede02
KavyaRajendran <kavyarrajendran01@gmail.com>
2020-03-06 11:05:15 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //ARRAY METHODS
-
-let arr = [{name:'a'},{name:'b'},{name:'c'}];
+let Narr = [12,3,4,5,6];
+let arr = [{name:'a',dept'ece'},{name:'b'},{name:'c'}];
 for (let tempvar of arr)
 {
   console.log(tempvar);
@@ -14,6 +14,21 @@ for(let i=0;i<arr.length;i++)
 arr.forEach
 (element => console.log(element));
 
-arr.forEach(item,index,arr) {
+arr.forEach((item,index,arr)=> {
+  if(index>1)
   console.log(item);
-}
+})
+
+const filter = arr.filter((num,i)=>i>1)
+
+//includes ,indexof() does not work for objects 
+//objects can be passed as
+const arr1 = console.log(Narr.includes(2));
+
+const arr1 = console.log(Narr.indexOf(3)); //primtive type
+
+
+const arr1 = console.log(Narr.findIndex(item=>item >2)); //object
+
+const arr1 = console.log(arr.find());
+Narr.sort((a,b)=>a-b)
